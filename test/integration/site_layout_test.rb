9e236f4cde50bb1df9a8255fc3fb5ab921c13b7f
Alex <lx.devr@gmail.com>
2016-03-03 20:31:16 +0000

    Ex 9.2: conditional testing of layout links (logged in/logged out) *Profile and settings link tests NOT working

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,12 +2,36 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "layout links without login" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
+    # assert_select "a[href=?]", @current_user, count: 0 #profile
+    # assert_select "a[href=?]", edit_user_path(@current_user), count: 0 #settings
+    assert_select "a[href=?]", users_path, count: 0
+    assert_select "a[href=?]", logout_path, count: 0
+  end
+
+  test "layout links after login" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path
+    # assert_select "a[href=?]", @current_user #profile
+    # assert_select "a[href=?]", edit_user_path(@current_user) #settings
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path, count: 0
   end
 end
