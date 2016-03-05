a4e8241eb2e33da5d872f66028db67e66797c6cc
Alex <lx.devr@gmail.com>
2016-03-05 00:30:38 +0000

    Ex 9.2: Finished tests for Profile and Settings links when logged in

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -27,8 +27,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", users_path
-    # assert_select "a[href=?]", @current_user #profile
-    # assert_select "a[href=?]", edit_user_path(@current_user) #settings
+    assert_select "a[href=?]", user_path(@user) #profile
+    assert_select "a[href=?]", edit_user_path(@user) #settings
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
